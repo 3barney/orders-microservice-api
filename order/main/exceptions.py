704e2171f232e2e098748719ca9976aec6cf68ed
barney <brnbsjm@gmail.com>
2019-01-16 16:31:14 +0300

    Add order models and model manager custom exceptions

---
+++ order/main/exceptions.py
@@ -0,0 +1,20 @@
+class InvalidArgumentError(Exception):
+
+  # With {argument_name} we can specify what caused exception to be raised
+  def __init__(self, argument_name):
+    message = f'The argument {argument_name} is invalid'
+    super().__init__(message) #Call constructor on superclass
+
+
+class OrderAlreadyCompletedError(Exception):
+
+  def __init__(self, order):
+    message = f'The order with ID {order} is already completed'
+    super().__init__(message)
+
+
+class OrderAlreadyCancelledError(Exception):
+
+  def __init__(self, order):
+    message = f'The order with ID {order} is already cancelled'
+    super().__init__(message)
--- order/main/models.py
+++ order/main/models.py
@@ -1,7 +1,37 @@
 from django.db import models
 
-# Create your models here.
-class OrderModels(models.Model):
+## Model that stores info about ordering customer 
+class OrderCustomer(models.Model):
   customer_id = models.IntegerField()
   name = models.CharField(max_length=100)
-  email = models.CharField(max_lenght=100)
+  email = models.CharField(max_length=100)
+
+
+## Model that stores info about customers order
+class Order(models.Model):
+  ORDER_STATUS = (
+    (1, 'Received'),
+    (2, 'Processing'),
+    (3, 'Payment complete'),
+    (4, 'Shipping'),
+    (5, 'Completed'),
+    (6, 'Cancelled'),
+  )
+
+  # define fKey (order_customer) creates Rltnshp between OrderCustomer n Order
+  order_customer = models.ForeignKey(OrderCustomer, on_delete=models.CASCADE)
+  totals = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+  created_at = models.DateTimeField(auto_now_add=True) #auto_now_add, this adds timezone info
+  last_updated = models.DateTimeField(auto_now=True)
+  status = models.IntegerField(choices=ORDER_STATUS, default='1')
+
+## Holds items belonging to an Order
+class OrderItems(models.Model):
+  class Meta: #Set metadata information on our Model class
+    verbose_name_plural = 'Order Items'
+
+  product_id = models.IntegerField()
+  name = models.CharField(max_length=200)
+  quantity = models.IntegerField()
+  price_per_unit = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+  order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
